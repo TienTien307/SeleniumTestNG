cb6c2632764445e70c61a2c093d0a151409e1495
Tiennt <Nguyenthuytien307@gmail.com>
2023-09-07 14:36:44 +0700

    testNG framework

---
+++ src/test/java/Bai9/TestNGFramework/RunTestNG02.java
@@ -0,0 +1,47 @@
+package Bai9.TestNGFramework;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class RunTestNG02 {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void createDriver() {
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+    }
+
+    @Test
+    public void testAnhTesterBlog() throws InterruptedException {
+        driver.get("https://anhtester.com");
+        driver.findElement(By.xpath("//a[normalize-space()='blog']")).click();
+        Thread.sleep(1000);
+    }
+
+    @Test
+    public void testGoogleSearch() throws InterruptedException {
+        driver.get("https://www.google.com/");
+        driver.findElement(By.xpath("//textarea[@id='APjFqb']")).sendKeys("anhtester", Keys.ENTER);
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("(//h3[normalize-space()='Anh Tester Automation Testing'])[1]")).click();
+        Thread.sleep(1000);
+    }
+
+    @AfterMethod
+    public void closeDriver() {
+        driver.quit();
+    }
+}
