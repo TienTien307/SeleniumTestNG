f6a95d1ec0f3f3cb96648ce8c5998c518596b1e4
Tiennt <Nguyenthuytien307@gmail.com>
2023-09-11 11:54:49 +0700

    //bài tập 1+2

--- src/test/java/common/BaseTest.java
+++ src/test/java/common/BaseTest.java
@@ -3,7 +3,9 @@ package common;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 import java.time.Duration;
@@ -11,7 +13,7 @@ import java.time.Duration;
 public class BaseTest {
     public WebDriver driver;
 
-    @BeforeMethod
+    @BeforeClass
     public void createBrowser(){
         System.out.println("Start Chrome browser from basetest");
         WebDriverManager.chromedriver().setup();
@@ -21,9 +23,9 @@ public class BaseTest {
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
     }
 
-    @AfterMethod
-    public void closeBrowser(){
-        System.out.println("Close browser");
-        driver.quit();
-    }
+//    @AfterClass
+//    public void closeBrowser(){
+//        System.out.println("Close browser");
+//        driver.quit();
+//    }
 }
